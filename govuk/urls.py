d078020d1ce639b8d583d819afdd212340664c7d
OllieJC <5426038+OllieJC@users.noreply.github.com>
2026-02-27 13:29:06 +0000

    Bump version

--- govuk/urls.py
+++ govuk/urls.py
@@ -81,8 +81,6 @@ if settings.FEATURE_FLAGS.get("FEEDBACK"):
         path("feedback/", feedback_view, name="feedback"),
     ]
 
-
-# Serve static and media files from development server
 urlpatterns += staticfiles_urlpatterns()
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
