d2b41a56b6d858c2120c6574284edb61382dd09f
OllieJC <5426038+OllieJC@users.noreply.github.com>
2026-02-27 13:52:12 +0000

    Maybe order of imports matter?

--- govuk/urls.py
+++ govuk/urls.py
@@ -2,9 +2,6 @@ from django.conf import settings
 from django.urls import include, path
 from django.contrib import admin
 
-from django.conf.urls.static import static
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-
 from wagtail.admin import urls as wagtailadmin_urls
 from wagtail import urls as wagtail_urls
 from wagtail.documents import urls as wagtaildocs_urls
@@ -80,6 +77,10 @@ if settings.FEATURE_FLAGS.get("FEEDBACK"):
         path("feedback", feedback_view),
         path("feedback/", feedback_view, name="feedback"),
     ]
+
+
+from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns += staticfiles_urlpatterns()
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
