f58200c5b7bf509da8b57c652b0c09bd1e7ce3f5
Edward Lonsdale <edward.lonsdale@digital.cabinet-office.gov.uk>
2026-03-03 06:18:27 +0000

    feat: bump version

--- govuk/settings/base.py
+++ govuk/settings/base.py
@@ -19,7 +19,7 @@ from pathlib import Path
 
 from django.utils.csp import CSP
 
-VERSION = "7.3-055"
+VERSION = "7.3-056"
 
 PROJECT_DIR = Path(__file__).resolve().parent.parent
 BASE_DIR = PROJECT_DIR.parent
