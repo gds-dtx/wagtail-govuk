51264e86b5ba9304186944693241844208511c3a
OllieJC <5426038+OllieJC@users.noreply.github.com>
2026-04-20 12:39:12 +0100

    Better defaults for security features

--- govuk/settings/local.py
+++ govuk/settings/local.py
@@ -8,6 +8,9 @@ WHITENOISE_USE_FINDERS = True
 INCOMING_REQUEST_INFO_LOGGING = True
 CONTENT_DISCOVERY_REQUEST_INFO_LOGGING = True
 
+SECURE_HSTS_SECONDS = 0
+SESSION_COOKIE_SECURE = False
+
 # This is a secret key used for cryptographic signing and is not suitable
 # for production use. This govuk.settings.local is for local development only.
 SECRET_KEY = "abc123"
